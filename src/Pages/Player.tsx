510678f2d7e2a2eff808082cec57f1d1b631d1eb
bekmurodovogabek0607 <126815147+bekmurodovogabek0607@users.noreply.github.com>
2023-04-29 01:07:32 -0700

    Update Player.tsx

--- src/Pages/Player.tsx
+++ src/Pages/Player.tsx
@@ -42,13 +42,13 @@ const Player = () => {
       props?.setAudio(nums - 1)
     }
   }
-  useEffect(() => {
+  setInterval(() => {
     setCurrentTime(props?.audioRef.current?.currentTime as number)
     if (Repeat) {
       RepeatAll()
     }
 
-  }, [CurrentTime])
+  }, 10)
   let After: number = props?.SuraNumber as number
   let Before: number = props?.SuraNumber as number
   function RepeatAll() {
